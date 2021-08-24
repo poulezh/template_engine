6479409e467f3fd9dbb03798dc7963e7dda5be22
poulezh <ezh1986@inbox.ru>
2021-08-25 00:41:19 +0700

    удалил почти весь код,переделал ф-ии

--- js/app.js
+++ js/app.js
@@ -4,121 +4,42 @@ import measuring_system from './Components/measuring_system.js'
 
 let errors = errorWidget();
 let measuring = measuring_system()
-
 let _inch_ = measuring
-let create_option = document.forms.create_option
-let input = create_option.elements.name
 
-let spisok = document.querySelector('#spisok')
 
-spisok.addEventListener('change', () => {
-  if (spisok.value === 'create') {
-    create_option.classList.remove('hide')
-  }
-  else {
-    create_option.classList.add('hide')
-  }
-})
 
-function selectOne(name) {
-  // console.log(spisok.length);
-  let nums = spisok.length - 1
+let one_form = document.forms.form_one
+
+function selectOne(name, sel) {
+  let nums = sel.length - 1
   let num = nums + 1
-  let options = spisok.options
+  let options = sel.options
   options[num] = new Option(`${name}`, `${name}`);
-  input.value = ''
-  // console.log(options[num].value);
-  // let createOption = Array.from(options)
-  // console.log(createOption);
+  name = ''
 }
 
-function selOne(name) {
-
-  // let nums = select.length -1
-  // let num = nums +1
-  let options = select.options
-  options[num] = new Option(`${name}`, `${name}`);
-  input.value = ''
-}
-
-
-
-create_option.addEventListener('click', function (e) {
+const create_option = (form) => (e) => {
   e.preventDefault()
-  let btn = create_option.elements.btn
+  let btn = form.elements.btn
+  let input = form.elements.input
+  let select = form.elements.select
   let target = e.target
   if (target == btn) {
     if (!input.value == '') {
       console.log('Нормас');
-      selectOne(input.value)
-      create_option.classList.add('hide')
+      let imp_val = input.value
+      selectOne(imp_val, select)
+      form.reset()
     }
     else {
       console.log(errors);
     }
   }
-})
-
-// console.log(inch);
-
-
-
-
-
-function one_page() {
-  let one_form = document.forms.form_one
-  // console.log(one_form);
-  let input = one_form.inpt_create
-  input.addEventListener('input', inpt_val)
-  let select = one_form.select_one
-
-  function inpt_val() {return input.value}
-
-  function selOne(name) {
-    let num = select.length
-    let options = select.options
-    options[num] = new Option(`${name}`, `${name}`);
-    input.value = ''
-  }
-
-  one_form.addEventListener('click', function (e) {
-    e.preventDefault()
-    let btn = one_form.elements.btn_create
-    let target = e.target
-    if (target == btn) {
-      if (!input.value == '') {
-        console.log('Нормас');
-        selOne(input.value)
-
-      }
-      else {
-        console.log(errors);
-      }
-    }
-  })
-  let arr_one_page = {
-    'one_form': one_form,
-    'input': input,
-    'select': select
-  }
-  return arr_one_page
 }
- let one_page_data = one_page()
-console.log(one_page_data.input.value);
 
-function bolte (){
-  console.log(one_page_data.select.length);
-}
-bolte()
+let handler_one_form = create_option(one_form)
 
-
-
-
-
-
-
-
-
+one_form.addEventListener('click',handler_one_form)
 
 
 
